7c13ad574b9441a2134e639a0c37fa50131d5268
Prashant Sahni <sahni.prashantkumar.externe@prosodie.com>
2020-06-23 21:54:28 +0530

    Main page desig

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,7 +12,7 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Play Boys Near You
         </q-toolbar-title>
 
         <div>
@@ -33,7 +33,7 @@
           header
           class="text-grey-8"
         >
-          Essential Links
+          Contact By City
         </q-item-label>
         <EssentialLink
           v-for="link in essentialLinks"
@@ -69,46 +69,16 @@ export default {
       leftDrawerOpen: false,
       essentialLinks: [
         {
-          title: 'Docs',
+          title: 'Delhi',
           caption: 'quasar.dev',
           icon: 'school',
           link: 'https://quasar.dev'
         },
         {
-          title: 'Github',
+          title: 'Mumbai',
           caption: 'github.com/quasarframework',
           icon: 'code',
           link: 'https://github.com/quasarframework'
-        },
-        {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
-        },
-        {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
-        },
-        {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
-        },
-        {
-          title: 'Quasar Awesome',
-          caption: 'Community Quasar projects',
-          icon: 'favorite',
-          link: 'https://awesome.quasar.dev'
         }
       ]
     }},methods:{
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,84 +1,79 @@
 <template>
-  <q-page class="flex flex-center">
-  <div class="row">
-    
-  <div class="col-12">
-    <h4>
-      APPLY FOR MALE ESCORT JOB JOIN OUR GIGOLO CLUB PLAYBOY AND CALLBOY JOINING INDIA</h4>
-   DesirePlayboys escort service aims to ensure its activities to be efficient, fast and discreet relationship between our members and clients. Our aim is to offer best possible service. We recommend you to make an appointment with our agents by just filling registration form. Now we also provide dating partner, dance partner, travel companion, part time boyfriend to fulfill your emotional as well as sexual needs. We joined with male escort directories, adult website, pornography, message centers, and other sexual services. For any queary you may visit our office or you can directly contact our agents.
+  <q-page>
+    <div class="row">
+      <div>
+        <h4 class='bold ub'>
+          APPLY FOR MALE ESCORT JOB JOIN OUR GIGOLO CLUB PLAYBOY AND CALLBOY JOINING INDIA
+        </h4>
+        DesirePlayboys escort service aims to ensure its activities to be efficient, fast and discreet relationship between our members and clients. Our aim is to offer best possible service. We recommend you to make an appointment with our agents by just filling registration form. Now we also provide dating partner, dance partner, travel companion, part time boyfriend to fulfill your emotional as well as sexual needs. We joined with male escort directories, adult website, pornography, message centers, and other sexual services. For any queary you may visit our office or you can directly contact our agents.
+        Now you can join as a male escort or a gigolo in all major cities in India including Ahmedabad, Bangalore, Bhubaneswar, Chandigarh, Chennai, Delhi, Goa, Hyderabad, Indore, Jaipur, Kerala, Kolkata, Lucknow, Mumbai, Pune, Surat and many more. Our rich clients prefers playboys as a part time boyfriend and call boys to fulfill sexual desire. You can also work as an independent escort and get paid from clients. DesirePlayboys is very reputed for providing Adult dating, friendship club and sex job services across India. Many hotels and tour agents also contact us to hire playboy and gigolo for NRI women.
+        All our male escorts are between 20-35 years of age, gigolos have a good phusique, playboys and callboys are very handsome and age between 20-27 years. They all are well presented with charm and fun personalities. Incall and outcall facilities are available with refreshments including soft drinks. To be a part of us you can simply fill-up call boy registration form. If you need more information you can visit our office or contact our agents.
+      </div>
+      <div class='flex'>
+        <q-card class="my-card" flat bordered>   
+          <h5 class='c-heading'>
+            Get Paid to Satisfy Lonely Ladies
+          </h5>
+          Market is booming, so many unmarried Ladies
+          looking for companionship
+          Market is booming, so many unmarried Ladies
+          looking for companionshipMarket is booming, so many unmarried Ladies
+          looking for companionshipMarket is booming, so many unmarried Ladies
+          looking for companionshipMarket is booming, so many unmarried Ladies
+          looking for companionship
+        </q-card>
+        <q-card class="my-card" flat bordered> 
 
-Now you can join as a male escort or a gigolo in all major cities in India including Ahmedabad, Bangalore, Bhubaneswar, Chandigarh, Chennai, Delhi, Goa, Hyderabad, Indore, Jaipur, Kerala, Kolkata, Lucknow, Mumbai, Pune, Surat and many more. Our rich clients prefers playboys as a part time boyfriend and call boys to fulfill sexual desire. You can also work as an independent escort and get paid from clients. DesirePlayboys is very reputed for providing Adult dating, friendship club and sex job services across India. Many hotels and tour agents also contact us to hire playboy and gigolo for NRI women.
+          <h5 class='c-heading'>  How to become Male Escort/Gigolo in India?</h5>
+          Step 1: Register with our website here Male escort registration Form.
+          Step 2: Upload Good Pictures and Real Informations.
+          Step 3: Verify your Email, Phone and upload Adhar card or Pan card to activate your Profile.
+          Step 4: You will get calls from Female clients looking for gigolo from your City.
+          Step 5: Attend the Meeting and get paid from the Client
+          Step 6: To get Better service and Dedicated help Contact our Agents.
 
-All our male escorts are between 20-35 years of age, gigolos have a good phusique, playboys and callboys are very handsome and age between 20-27 years. They all are well presented with charm and fun personalities. Incall and outcall facilities are available with refreshments including soft drinks. To be a part of us you can simply fill-up call boy registration form. If you need more information you can visit our office or contact our agents.
-  </div>
-  <div class="col-12" flex >
-  <q-card class="my-card" flat bordered> 
-    
-     <h5>
-      Get Paid to Satisfy Lonely Ladies
-        </h5>
-        Market is booming, so many unmarried Ladies
-        looking for companionship
-        Market is booming, so many unmarried Ladies
-        looking for companionshipMarket is booming, so many unmarried Ladies
-        looking for companionshipMarket is booming, so many unmarried Ladies
-        looking for companionshipMarket is booming, so many unmarried Ladies
-        looking for companionship
+        </q-card>
 
-  </q-card>
-<q-card class="my-card" flat bordered> 
+        <q-card class="my-card" flat bordered> 
+          <h5 class='c-heading'>  How to hire a callboy/playboy?</h5>
+            Step 1: Browse our website or click here to Search for male escort.
+            Step 2: Check their profile status before contacting any gigolo.
+            Step 3: Shortlist atleast 3-4 escorts then contact them and ask for availability.
+            Step 4: Fix a appointment and give them your address.
+            Step 5: Pay them after the job. and Post a review on their profile.
+            Step 6: To get Better service and Dedicated help Contact our Agents.
+        </q-card>
+        <q-card class="my-card" flat bordered> 
+          
+          <h5 class='c-heading'> Eligibility for Male Escort job</h5>
+            1. Candidate should know HINDI or ENGLISH.
+            2. Candidate should be well mannered and decent.
+            3. Candidate has to dressed up according to occasions.
+            4. Candidate needs to be clean and hygienic.
+            5. Candidate must not be addicted to drugs or alcohol.
+            6. Candidate must not be affected by S.T.D.
+        </q-card>
 
-  <h5>  How to become Male Escort/Gigolo in India?</h5>
-Step 1: Register with our website here Male escort registration Form.
-Step 2: Upload Good Pictures and Real Informations.
-Step 3: Verify your Email, Phone and upload Adhar card or Pan card to activate your Profile.
-Step 4: You will get calls from Female clients looking for gigolo from your City.
-Step 5: Attend the Meeting and get paid from the Client
-Step 6: To get Better service and Dedicated help Contact our Agents.
-
-</q-card>
-
- <q-card class="my-card" flat bordered> 
-  <h5>  How to hire a callboy/playboy?</h5>
-Step 1: Browse our website or click here to Search for male escort.
-Step 2: Check their profile status before contacting any gigolo.
-Step 3: Shortlist atleast 3-4 escorts then contact them and ask for availability.
-Step 4: Fix a appointment and give them your address.
-Step 5: Pay them after the job. and Post a review on their profile.
-Step 6: To get Better service and Dedicated help Contact our Agents.
- </q-card>
- <q-card class="my-card" flat bordered> 
-   
-  <h5> Eligibility for Male Escort job</h5>
-1. Candidate should know HINDI or ENGLISH.
-2. Candidate should be well mannered and decent.
-3. Candidate has to dressed up according to occasions.
-4. Candidate needs to be clean and hygienic.
-5. Candidate must not be addicted to drugs or alcohol.
-6. Candidate must not be affected by S.T.D.
- </q-card>
-
- <q-card class="my-card" flat bordered> 
-  <h5> Why join our gigolo club?</h5>
-1. We provide 100% verified profile
-2. We offer an affordable starting.
-3. We don’t share our member’s privacy to anyone.
-4. We don’t share our client’s privacy to anyone.
-5. We are not attending unknown clients.
-6. We are not attending STD affected clients
- </q-card>
-  <q-card class="my-card" flat bordered> 
-  <h5> Why join DesirePlayboys.com?</h5>
-1. We never Share Our User data to anyone.
-2. All the Profile are Verified and secured.
-3. 100% Money back guarentee.
-4. Spam and Froud Prevention.
-5. 24*7 Costmercare assistance
-6. Dedicated Agents and Manage
-  </q-card>
-  </div>
-  </div>
-  
+        <q-card class="my-card" flat bordered> 
+          <h5 class='c-heading'> Why join our gigolo club?</h5>
+            1. We provide 100% verified profile
+            2. We offer an affordable starting.
+            3. We don’t share our member’s privacy to anyone.
+            4. We don’t share our client’s privacy to anyone.
+            5. We are not attending unknown clients.
+            6. We are not attending STD affected clients
+        </q-card>
+        <q-card class="my-card" flat bordered> 
+          <h5 class='c-heading'> Why join DesirePlayboys.com?</h5>
+            1. We never Share Our User data to anyone.
+            2. All the Profile are Verified and secured.
+            3. 100% Money back guarentee.
+            4. Spam and Froud Prevention.
+            5. 24*7 Costmercare assistance
+            6. Dedicated Agents and Manage
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
